2bc4664e83f4dc4913889e54c2ec5a19849c0922
Amory <108595086+amorychambers@users.noreply.github.com>
2024-08-21 14:16:29 +0100

    Initial commit

--- 010_comments.py
+++ 010_comments.py
@@ -85,6 +85,8 @@
 
 # Type your name as a comment on the next line.
 
+# Amory here, and a huge fan of opening with Calvino. Classy! 
+
 # Hint: if you're on a Mac, type opt + 3 to get a #
 
 # Now open up 011_identity.py and get started with some
